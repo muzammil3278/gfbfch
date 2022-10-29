108800a14ae9528dc6a974ef4e9d64b61f3abada
muzammil3278 <mmuzammil528982@gmail.com>
2022-10-29 11:41:38 +0500

    gh

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 export default function Blog({ posts }) {
   return (
-    <>vd</>
-    // <ul>
-    //   {posts.map((post) => (
-    //     <div key={post.id}>
-    //       <li>{post.body}</li>
-    //     </div>
-    //   ))}
-    // </ul>
+    // <>vd</>
+    <ul>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <li>{post.body}</li>
+        </div>
+      ))}
+    </ul>
   )
 }
 // This function gets called at build time
-// export async function getStaticProps() {
-//   // Call an external API endpoint to get posts
-//   const res = await fetch('http://localhost:3004/comments')
-//   const posts = await res.json()
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('http://localhost:3004/comments')
+  const posts = await res.json()
 
-//   // By returning { props: { posts } }, the Blog component
-//   // will receive `posts` as a prop at build time
-//   return {
-//     props: {
-//       posts,
-//     },
-//   }
-// }
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
+}
